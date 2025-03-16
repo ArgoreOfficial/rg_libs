6d7ac2ff6090a3abc010ab4008255ff045dffac6
Argore <argoreyt@gmail.com>
2025-03-16 17:50:09 +0100

    triangle clipping

--- CPU0.lua
+++ CPU0.lua
@@ -23,55 +23,35 @@ rg3d:push_perspective(
 local screen_width  = gdt.VideoChip0.Width
 local screen_height = gdt.VideoChip0.Height
 
-print(       vec2( 1.2, 3.4           ) )
-print( rmath:vec4( 1.2, 3.4, 5.6, 7.8 ) )
+local vertex_data = {
+	-- bottom quad
+	rmath:vec4(-0.5, -0.5, 0.0, 1),
+	rmath:vec4(-0.5,  0.5, 0.0, 1),
+	rmath:vec4( 0.5, -0.5, 0.0, 1),
 
-local function line_intersect_plane(_start, _end, _plane_pos, _plane_normal )
-	_plane_normal = rmath:vec3_normalize(_plane_normal)
-	local plane_d = -rmath:vec3_dot(_plane_normal,_plane_pos)
-	local ad = rmath:vec3_dot(_start, _plane_normal)
-	local bd = rmath:vec3_dot(_end, _plane_normal)
-	local t = (-plane_d - ad) / (bd - ad)
-	
-	if t < 0 or t > 1 then return nil end
-	
-	local start_to_end = _end - _start
-	local line_to_intersect = start_to_end * t
-	
-	return _start + line_to_intersect
-end
+	rmath:vec4(-0.5,  0.5, 0.0, 1),
+	rmath:vec4( 0.5,  0.5, 0.0, 1),
+	rmath:vec4( 0.5, -0.5, 0.0, 1),
+
+	-- top quad
+	rmath:vec4(-0.5, -0.5 + 1, 0.0, 1),
+	rmath:vec4(-0.5,  0.5 + 1, 0.0, 1),
+	rmath:vec4( 0.5, -0.5 + 1, 0.0, 1),
+
+	rmath:vec4(-0.5,  0.5 + 1, 0.0, 1),
+	rmath:vec4( 0.5,  0.5 + 1, 0.0, 1),
+	rmath:vec4( 0.5, -0.5 + 1, 0.0, 1)
+}
 
 -- update function is repeated every time tick
 function update()
 	gdt.VideoChip0:Clear(color.black)
-
-
+	
+	local s = math.sin(gdt.CPU0.Time * 0.3)
+	rg3d:push_look_at(vec3(1.5,0,2), vec3(0,s*3,0), vec3(0,1,0))
+	
 	-- draw triangles
-	do
-		-- in world space
-		local vertex_data = {
-			-- bottom quad
-			rmath:vec4(-0.5, -0.5, 0.0, 1),
-			rmath:vec4(-0.5,  0.5, 0.0, 1),
-			rmath:vec4( 0.5, -0.5, 0.0, 1),
-	
-			rmath:vec4(-0.5,  0.5, 0.0, 1),
-			rmath:vec4( 0.5,  0.5, 0.0, 1),
-			rmath:vec4( 0.5, -0.5, 0.0, 1),
-	
-			-- top quad
-			rmath:vec4(-0.5, -0.5 + 1, 0.0, 1),
-			rmath:vec4(-0.5,  0.5 + 1, 0.0, 1),
-			rmath:vec4( 0.5, -0.5 + 1, 0.0, 1),
-	
-			rmath:vec4(-0.5,  0.5 + 1, 0.0, 1),
-			rmath:vec4( 0.5,  0.5 + 1, 0.0, 1),
-			rmath:vec4( 0.5, -0.5 + 1, 0.0, 1)
-		}
-		
-		local s = math.sin(gdt.CPU0.Time * 0.3)
-		rg3d:push_look_at(vec3(1.5,0,2), vec3(0,s*3,0), vec3(0,1,0))
-		
+	for i=1,100 do -- stress test of how many can be rasterized at once
 		-- raster
 		local p1, p2, p3
 		for i = 1, #vertex_data, 3 do
@@ -79,36 +59,6 @@ function update()
 			p2 = vertex_data[i + 1]
 			p3 = vertex_data[i + 2]
 			rg3d:raster_triangle({p1,p2,p3},screen_width,screen_height)
-		end
-	end
-
-	-- line intersect test
-	do
-		local lineX = math.cos(gdt.CPU0.Time * 3) * 50 + 60
-		local lineY = math.sin(gdt.CPU0.Time * 3) * 70 + 80
-		local lineA = vec3(0,0,0)
-		local lineB = vec3(lineX,lineY,0)
-		local plane = vec3(50,0,0)
-		local plane_n = vec3(1,0,0)
-		local intersect = line_intersect_plane(lineA,lineB,plane,plane_n)
-
-		local last_pos = lineB
-		local trail_segments = 20
-		local trail_len = 0.5
-		for i=0,trail_segments do
-			local t = gdt.CPU0.Time - (i / trail_segments * trail_len)
-			local pX = math.cos(t * 3) * 50 + 60
-			local pY = math.sin(t * 3) * 70 + 80
-			local pos = vec2(pX,pY)
-			gdt.VideoChip0:DrawLine(last_pos, pos, color.white)
-			last_pos = pos
-		end
-
-		gdt.VideoChip0:DrawLine(lineA,lineB, color.red)
-		gdt.VideoChip0:DrawLine(vec2(50,0),vec2(50,screen_height), color.green)
-		if intersect ~= nil then
-			gdt.VideoChip0:FillCircle(intersect,3,color.black)
-			gdt.VideoChip0:DrawCircle(intersect,3,color.white)
 		end
 	end
 end
--- rg_3d.lua
+++ rg_3d.lua
@@ -52,58 +52,119 @@ function lib:to_screen(_vec, _screen_width, _screen_height)
 	return vec3(sv4.X, sv4.Y, sv4.Z)
 end
 
-local function count_inside_value(_v1,_v2,_v3,_min,_max)
-	local min = _min or -1
-	local max = _max or  1
-	
-	local p1 = (_v1 >= min and _v1 < max) and 1 or 0
-	local p2 = (_v2 >= min and _v2 < max) and 1 or 0
-	local p3 = (_v3 >= min and _v3 < max) and 1 or 0
+local function dist_to_plane(_point,_plane_pos,_plane_normal)
+	local n = rmath:vec3_normalize(_point)
+	return _plane_normal.X * _point.X
+	     + _plane_normal.Y * _point.Y
+		 + _plane_normal.Z * _point.Z
+		 - rmath:vec3_dot(_plane_normal, _plane_pos)
+end
 
-	local v = p1 + p2 + p3
-	return v
+local function line_intersect_plane(_start, _end, _plane_pos, _plane_normal )
+	_plane_normal = rmath:vec3_normalize(_plane_normal)
+	local plane_d = -rmath:vec3_dot(_plane_normal,_plane_pos)
+	local ad = rmath:vec3_dot(_start, _plane_normal)
+	local bd = rmath:vec3_dot(_end, _plane_normal)
+	local t = (-plane_d - ad) / (bd - ad)
+	
+	if t < 0 or t > 1 then return nil end
+	
+	local start_to_end = _end - _start
+	local line_to_intersect = start_to_end * t
+	
+	return _start + line_to_intersect
+end
+
+local function triangle_clip_plane(_tri, _plane_pos, _plane_normal)
+	_plane_normal = rmath:vec3_normalize(_plane_normal)
+	local inside_points = {}
+	local outside_points = {}
+
+	local d1 = dist_to_plane(_tri[1], _plane_pos, _plane_normal)
+	local d2 = dist_to_plane(_tri[2], _plane_pos, _plane_normal)
+	local d3 = dist_to_plane(_tri[3], _plane_pos, _plane_normal)
+
+	if d1 >= 0 then inside_points [#inside_points +1] = _tri[1]
+	else            outside_points[#outside_points+1] = _tri[1] end
+
+	if d2 >= 0 then inside_points [#inside_points +1] = _tri[2]
+	else            outside_points[#outside_points+1] = _tri[2] end
+
+	if d3 >= 0 then inside_points [#inside_points +1] = _tri[3]
+	else            outside_points[#outside_points+1] = _tri[3] end
+
+	if #inside_points == 0 then return nil, nil, nil end
+	if #inside_points == 3 then return _tri, nil, nil end
+
+	if #inside_points == 1 and #outside_points == 2 then
+		local t = {
+			inside_points[1],
+			line_intersect_plane(inside_points[1], outside_points[1], _plane_pos, _plane_normal),
+			line_intersect_plane(inside_points[1], outside_points[2], _plane_pos, _plane_normal)
+		}
+		
+		return t, nil, nil
+	end
+
+	
+	if #inside_points == 2 and #outside_points == 1 then
+		local t1 = {
+			inside_points[1],
+			inside_points[2],
+			line_intersect_plane(inside_points[1], outside_points[1], _plane_pos, _plane_normal)
+		}
+		
+		local t2 = {
+			inside_points[2],
+			t1[3],
+			line_intersect_plane(inside_points[2], outside_points[1], _plane_pos, _plane_normal)
+		}
+		
+		return t1, t2, nil
+	end
+end
+
+local function clip_triangles_plane(_in, _out,_plane_pos,_plane_normal)
+	for i=1,#_in do
+		local a,b,c = triangle_clip_plane(_in[i],_plane_pos,_plane_normal)
+		if a then _out[#_out+1] = a end
+		if b then _out[#_out+1] = b end
+		if c then _out[#_out+1] = c end
+	end
+end
+
+local function clip_triangles(_in, _out)
+	local temp, temp2 = {}, {}
+	local d = 0.9
+
+	clip_triangles_plane(_in, temp,   vec3(-d, 0, 0), vec3( 1, 0, 0)) -- left
+	clip_triangles_plane(temp, temp2, vec3( 0,-d, 0), vec3( 0, 1, 0)) -- top
+	temp = {}
+	clip_triangles_plane(temp2, temp, vec3( d, 0, 0), vec3(-1, 0, 0)) -- right
+	clip_triangles_plane(temp, _out,  vec3( 0, d, 0), vec3( 0,-1, 0)) -- bottom
 end
 
 function lib:raster_triangle(_tri, _render_width, _render_height)
-
 	local p1 = lib:to_clip(_tri[1])
 	local p2 = lib:to_clip(_tri[2])
 	local p3 = lib:to_clip(_tri[3])
 	
-	local x = count_inside_value(p1.X, p2.X, p3.X)
-	local y = count_inside_value(p1.Y, p2.Y, p3.Y)
-	local z = count_inside_value(p1.Z, p2.Z, p3.Z, 0, 1)
+	do return end
 
-	local count = math.min(x,y)	
-	
-	-- triangle is outside clip space
-	if count == 0 then
-		return
-	end
+	local triangle = {p1,p2,p3}
+	local tri_list = {triangle}
+	local clipped_tri_list = {}
+	clip_triangles(tri_list, clipped_tri_list)
 
-	-- triangle is inside clip space
-	if count == 3 then
-		gdt.VideoChip0:DrawTriangle(
-			rmath:vec3_to_screen(p1, _render_width, _render_height),
-			rmath:vec3_to_screen(p2, _render_width, _render_height),
-			rmath:vec3_to_screen(p3, _render_width, _render_height),
-			color.green )
-		return
-	end
-
-	-- triangle intersects with clip walls
 	local col = color.green
-	if count == 1 then
-		col = color.red
-	elseif count == 2 then
-		col = color.yellow
-	end
-
-	gdt.VideoChip0:DrawTriangle(
-			rmath:vec3_to_screen(p1, _render_width, _render_height),
-			rmath:vec3_to_screen(p2, _render_width, _render_height),
-			rmath:vec3_to_screen(p3, _render_width, _render_height),
+	
+	for i=1, #clipped_tri_list do
+		gdt.VideoChip0:DrawTriangle(
+			rmath:vec3_to_screen(clipped_tri_list[i][1], _render_width, _render_height),
+			rmath:vec3_to_screen(clipped_tri_list[i][2], _render_width, _render_height),
+			rmath:vec3_to_screen(clipped_tri_list[i][3], _render_width, _render_height),
 			col )
+	end
 end
 
 return lib
