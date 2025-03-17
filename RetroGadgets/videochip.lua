420cea375d0c1d19aa57644e30e73758ddfce173
Argore <argoreyt@gmail.com>
2025-03-17 10:44:36 +0100

    RasterSprite

--- RetroGadgets/videochip.lua
+++ RetroGadgets/videochip.lua
@@ -4,6 +4,19 @@
 local methods = {}
 local meta = {__index = methods}
 
+
+local draw_quad_data = {
+	{0,0, 0,0, 1,1,1,1},
+	{1,0, 1,0, 1,1,1,1},
+	{1,1, 1,1, 1,1,1,1},
+	{0,1, 0,1, 1,1,1,1},
+}
+local draw_quad = love.graphics.newMesh(draw_quad_data,"fan","dynamic")
+
+local function _color_to_rgba(_color)
+    return _color.R/255, _color.G/255, _color.B/255, _color.A/255
+end
+
 local function _set_color(_color)
     love.graphics.setColor(_color.R/255, _color.G/255, _color.B/255, _color.A/255)
 end
@@ -20,9 +33,7 @@ function _G._videochip()
 end
 
 function methods:Clear(_color)
-    local r = _color.R / 255.0
-    local g = _color.G / 255.0
-    local b = _color.B / 255.0
+    local r,g,b = _color_to_rgba(_color)
     love.graphics.clear(r,g,b)
 end
 
@@ -87,7 +98,18 @@ end
 
 function methods:DrawCustomSprite(position, spriteSheet, spriteOffset, spriteSize, tintColor, backgroundColor) error("unimplemented") end
 function methods:DrawText(position, fontSprite, text, textColor, backgroundColor) error("unimplemented") end
-function methods:RasterSprite(position1, position2, position3, position4, spriteSheet, spriteX, spriteY, tintColor, backgroundColor) error("unimplemented") end
+
+function methods:RasterSprite(_position1, _position2, _position3, _position4, _spriteSheet, _spriteX, _spriteY, _tintColor, _backgroundColor)
+    local r,g,b,a = _color_to_rgba(_tintColor)
+    draw_quad:setVertex(1, _position1.X, _position1.Y, 0.0, 0.0, r, g, b, a)
+    draw_quad:setVertex(2, _position2.X, _position2.Y, 1.0, 0.0, r, g, b, a)
+    draw_quad:setVertex(3, _position3.X, _position3.Y, 1.0, 1.0, r, g, b, a)
+    draw_quad:setVertex(4, _position4.X, _position4.Y, 0.0, 1.0, r, g, b, a)
+    draw_quad:setTexture(_spriteSheet._Image)
+
+    love.graphics.draw(draw_quad,0,0)
+end
+
 function methods:RasterCustomSprite(position1, position2, position3, position4, spriteSheet, spriteOffset, spriteSize, tintColor, backgroundColor) error("unimplemented") end
 function methods:DrawRenderBuffer(position, renderBuffer, width, height) error("unimplemented") end
 function methods:RasterRenderBuffer(position1, position2 , position3, position4, renderBuffer) error("unimplemented") end
