94cbaa29dffec81aefb11d58dc3a10e828050da7
Argore <argoreyt@gmail.com>
2025-03-20 09:37:04 +0100

    optional image.lua meta file

--- RetroGadgets/spritesheet.lua
+++ RetroGadgets/spritesheet.lua
@@ -6,11 +6,12 @@ function methods:IsValid()
 end
 
 function _G._spritesheet(_name,_image,_w,_h)
+    local r,m = pcall(require, _name:match("(.+)%..+") )
     return setmetatable({
         Name = "",
         Type = "",
         Palette = {},
-        _Meta = _name and require( _name:match("(.+)%..+") ) or nil,
+        _Meta = _name and (r and m or nil) or nil,
         _Image = _image,
         _Quad = _image and love.graphics.newQuad(0, 0, math.min(_w, 256), math.min(_h, 64), _image) or nil
     }, meta)
