2c8687eb1cdb0c44b824e2d44628cdd02cad0565
Argore <argoreyt@gmail.com>
2025-11-29 20:03:29 +0100

    ColorHSV

--- RetroGadgets/color.lua
+++ RetroGadgets/color.lua
@@ -8,6 +8,26 @@ function _G.Color(r,g,b)
     return ColorRGBA(r,g,b,255)
 end
 
+-- https://github.com/iskolbin/lhsx/blob/master/hsx.lua#L40
+function _G.ColorHSV(h, s, v)
+    local C = v * s
+	local m = v - C
+	local r, g, b = m, m, m
+	
+    local h2 = ((h / 360) % 1.0) * 6
+    local X = C * (1 - math.abs(h2 % 2 - 1))
+    C, X = C + m, X + m
+    if     h2 < 1 then r, g, b = C, X, m
+    elseif h2 < 2 then r, g, b = X, C, m
+    elseif h2 < 3 then r, g, b = m, C, X
+    elseif h2 < 4 then r, g, b = m, X, C
+    elseif h2 < 5 then r, g, b = X, m, C
+    else               r, g, b = C, m, X
+    end
+	
+	return Color(r * 255, g * 255, b * 255)
+end
+
 function color_meta.__eq(_lhs,_rhs)
     return 
         _lhs.R == _rhs.R and
